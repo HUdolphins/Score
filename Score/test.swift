b0756dc50e99b2697040f45ac56948f92b7cf5f3
fathy000 <caxki000@gmail.com>
2018-09-13 16:58:10 +0900

    test2

---
+++ Score/test.swift
@@ -0,0 +1,13 @@
+//
+//  test.swift
+//  Score
+//
+//  Created by Kazuki Ohashi on 2018/09/13.
+//  Copyright © 2018年 Kazuki Ohashi. All rights reserved.
+//
+
+import UIKit
+
+class test: NSObject {
+
+}
