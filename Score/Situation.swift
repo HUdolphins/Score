86995f27bc962196bde60c10d06d58bc91c71b85
kazufumi <n.k199772@gmail.com>
2018-09-13 16:44:32 +0900

    aaa

---
+++ Score/Situation.swift
@@ -0,0 +1,13 @@
+//
+//  Situation.swift
+//  Score
+//
+//  Created by 野村 和史 on 2018/09/13.
+//  Copyright © 2018年 Kazuki Ohashi. All rights reserved.
+//
+
+import UIKit
+
+class Situation: NSObject {
+
+}
